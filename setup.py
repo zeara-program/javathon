348a360adeda15d5bf6176c1c6e05a5a387bb688
zearakun <91083604+zearakun@users.noreply.github.com>
2022-05-12 11:18:03 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='package template',
+    description="package template",
+    version='0.0.1',
+    packages=find_packages(),
+    author="zeara-program",
+    license='MIT'
+)
